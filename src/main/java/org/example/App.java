2c972fcf92f1823299f36f6d4b05ceb2adb7d78f
Igor <mahotinigor@gmail.com>
2022-12-14 14:10:46 +0700

    Start Project

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        var context = new AnnotationConfigApplicationContext();
+        Parrot parrot = new Parrot();
+        System.out.println( "Hello World!" );
+    }
+}
